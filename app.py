1ffbc541933520a7f9274e87d706e3127567a724
Tarun-kumar-singh <tarunks2512@gmail.com>
2025-02-18 18:06:08 +0530

    fc

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+guest_namespace = '/guest'
+
+@socketio.on('connect', namespace=guest_namespace)
+def handle_connect():
+    print(f"A guest connected: {request.sid}")   
+
+@socketio.on('audioStream', namespace=guest_namespace)
+def handle_audio_stream(audio_chunk):
+    print(audio_chunk)
+    socketio.emit('streamBack', audio_chunk, namespace=guest_namespace)
+
+@socketio.on('stopRecording', namespace=guest_namespace)
+def handle_stop_recording():
+    print(f"Stopping recording for {request.sid}")   
+
+@socketio.on('disconnect', namespace=guest_namespace)
+def handle_disconnect():
+    print(f"User disconnected: {request.sid}")   
+
+# Define a GET route
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello, World!"
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    name = request.args.get('name', 'Guest')  # Default value is 'Guest' if no name is provided
+    return f"Hello, {name}!"
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=2000)
