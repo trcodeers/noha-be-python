bc576b894b52a3713573c1514ae127529818ff00
Tarun-kumar-singh <tarunks2512@gmail.com>
2025-02-18 21:25:38 +0530

    add logger

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def handle_connect():
 
 @socketio.on('audioStream', namespace=guest_namespace)
 def handle_audio_stream(audio_chunk):
-    print(audio_chunk)
     socketio.emit('streamBack', audio_chunk, namespace=guest_namespace)
 
 @socketio.on('stopRecording', namespace=guest_namespace)
@@ -27,17 +26,13 @@ def handle_stop_recording():
 
 @socketio.on('disconnect', namespace=guest_namespace)
 def handle_disconnect():
-    print(f"User disconnected: {request.sid}")   
+    logger.info(f"User disconnected: {request.sid}")   
 
 # Define a GET route
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello, World!"
 
-@app.route('/greet', methods=['GET'])
-def greet():
-    name = request.args.get('name', 'Guest')  # Default value is 'Guest' if no name is provided
-    return f"Hello, {name}!"
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=2000)
